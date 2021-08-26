a61f21e8929e8c7a35efdeb1f428788e9a3c429e
Logan <llbokilo@gmail.com>
2021-08-26 16:00:22 +0200

    Update PlaceController.php

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\Place;
+use App\Repository\PersonRepository;
 
 class PlaceController extends AbstractController
 {
@@ -33,11 +34,8 @@ class PlaceController extends AbstractController
     /**
      * @Route("/api/place/{id}", name="api_place_avec_id" , methods="GET")
      */
-    public function findById(
-        PlaceRepository $placeRepository,
-        $id,
-        NormalizerInterface $normalizer
-    ): Response {
+    public function findById(PlaceRepository $placeRepository, $id, NormalizerInterface $normalizer): Response
+    {
         $place = $placeRepository->find($id);
         $normalized = $normalizer->normalize($place, null, ['groups' => 'place:read']);
         $json = json_encode($normalized);
@@ -63,5 +61,20 @@ class PlaceController extends AbstractController
         } catch (NotEncodableValueException $e) {
             return $this->json(['status' => 400, 'message' => $e->getMessage()]);
         }
+    }
+
+    /**
+     * @Route("/api/place_like/", name="api_place_like",methods="POST")
+     */
+    public function like(EntityManagerInterface $entityManager,PlaceRepository $placeRepository,PersonRepository $personRepository,$idPlace,$idPerson)
+    {
+        $place = $placeRepository->find($idPlace);
+        $person = $personRepository->find($idPerson);
+
+        $place->addlikedby($person);
+
+        // $entityManager->persist($place); Pas necessaire
+        $entityManager->flush();
+        return $this->json($place, 201, [], ['groups' => 'place:read']);
     }
 }
